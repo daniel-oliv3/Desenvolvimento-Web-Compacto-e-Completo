d355a907a37fa567a612d0427cb34ca8897709f1
Daniel Oliveira <danielsapup3@gmail.com>
2023-03-20 09:02:41 -0400

    Detalhes Sobre Carregamento do JavaScript no HEAD do HTML

--- Seção 5 - JavaScript para DOM - Nível 1 - O básico do JavaScript numa página web/javascript_web-45/assets/js/script.js
+++ Seção 5 - JavaScript para DOM - Nível 1 - O básico do JavaScript numa página web/javascript_web-45/assets/js/script.js
@@ -9,21 +9,25 @@
 // console.log(document.readyState);
 
 
-document.addEventListener('readystatechange', (event) => {
-    // console.log(event.target.readyState);
-    if(event.target.readyState === 'complete'){
-        // este código vai ser executado antes dos elementos do body existire,
-        document.querySelector("button").addEventListener('click', () => {
-            document.querySelector('h1').innerText = "Texto do título alterado";
-        });
-    }
+// document.addEventListener('readystatechange', (event) => {
+//     // console.log(event.target.readyState);
+//     if(event.target.readyState === 'complete'){
+//         // este código vai ser executado antes dos elementos do body existire,
+//         document.querySelector("button").addEventListener('click', () => {
+//             document.querySelector('h1').innerText = "Texto do título alterado";
+//         });
+//     }
+// });
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector("button").addEventListener('click', () => {
+        document.querySelector('h1').innerText = "Texto do título alterado!";
+    });
 });
-
-
-
-
-
-
 
 
 
